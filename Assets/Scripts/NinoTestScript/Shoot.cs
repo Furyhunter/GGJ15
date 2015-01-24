d1d059c9ac22c96bc85e030259e76d0754016c26
Popolo <cvernino@tx.rr.com>
2015-01-23 21:42:03 -0600

    pre-refit

--- Assets/Scripts/NinoTestScript/Shoot.cs
+++ Assets/Scripts/NinoTestScript/Shoot.cs
@@ -7,9 +7,11 @@ public class Shoot : MonoBehaviour
 {
     public GameObject bullet;
     public int weapon_id;
-    const int proj_speed = 23;
+    const int proj_speed_default = 23;
     enum weapons {rifle, shotgun, rocket, fist_gun};
     private float fire_delay = 0.0f;
+
+    public LODGroup LodGroupTest;
 
     void Update ()
     {
@@ -23,13 +25,10 @@ public class Shoot : MonoBehaviour
             switch (weapon_id)
             {
             case (int)weapons.rifle:
-                for (int proj_count = 1; i > 0; --i)
-                {
-                    Vector3 proj_head = get_weapon_spread(4);
-                    GameObject new_bullet = (GameObject) Instantiate(bullet, transform.position, Quaternion.identity);
-                    new_bullet.GetComponent<Rigidbody>().velocity = proj_head * proj_speed;
-                }
-                fire_delay = 0.085f;
+                create_shots(bullet, 1, proj_speed_default, 4, 0.085f);
+                break;
+            case (int)weapons.shotgun:
+                create_shots(bullet, 7, proj_speed_default, 16, 0.75f);
                 break;
             default:
                 Debug.Log("Not a valid weapon.");
@@ -43,5 +42,15 @@ public class Shoot : MonoBehaviour
         float x_head = Mathf.Sin((transform.eulerAngles.y + (Random.value * 2 - 1) * max_degrees_offset) * Mathf.Deg2Rad);
         float z_head = Mathf.Cos((transform.eulerAngles.y + (Random.value * 2 - 1) * max_degrees_offset) * Mathf.Deg2Rad);
         return new Vector3(x_head, 0.0f, z_head);
+    }
+
+    void create_shots(GameObject ammunition, int proj_count, int proj_speed, int proj_spread, float refire_delay){
+    	for (int i = proj_count; i > 0; --i)
+                {
+                    Vector3 proj_head = get_weapon_spread(proj_spread);
+                    GameObject new_bullet = (GameObject) Instantiate(ammunition, transform.position, Quaternion.identity);
+                    new_bullet.GetComponent<Rigidbody>().velocity = proj_head * proj_speed;
+                }
+                fire_delay = refire_delay;
     }
 }
