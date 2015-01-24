7dbdbdf87ff5263d6f87482a32c63f5a35e369ef
Popolo <cvernino@tx.rr.com>
2015-01-24 11:05:24 -0600

    initial camera control.

---
+++ Assets/Scripts/NinoTestScript/CameraTargetControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTargetControl : MonoBehaviour
+{
+	private GameObject[] players;
+	void Update()
+	{
+		players = GameObject.FindGameObjectsWithTag("Player");
+		Vector3 avg = new Vector3(0,0,0);
+		foreach(GameObject e in players){
+			avg += e.transform.position;
+		}
+		avg.y = 0;
+		avg /= players.Length;
+	}
+}
