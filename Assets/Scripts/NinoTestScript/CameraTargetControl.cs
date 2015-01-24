20586b92e95d44fa70aea98fc06b84dc96de4eb0
Popolo <cvernino@tx.rr.com>
2015-01-24 11:51:29 -0600

    Camera Control added

--- Assets/Scripts/NinoTestScript/CameraTargetControl.cs
+++ Assets/Scripts/NinoTestScript/CameraTargetControl.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class CameraTargetControl : MonoBehaviour
 {
-	private GameObject[] players;
-	void Update()
-	{
-		players = GameObject.FindGameObjectsWithTag("Player");
-		Vector3 avg = new Vector3(0,0,0);
-		foreach(GameObject e in players){
-			avg += e.transform.position;
-		}
-		avg.y = 0;
-		avg /= players.Length;
-	}
+    private GameObject[] players;
+    void Update()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        if (players.Length != 0)
+        {	
+        	Vector3 avg = players[0].transform.position;
+            for (int i = 1; i < players.Length; ++i)
+            {
+                avg = Vector3.Lerp(avg, players[i].transform.position, 0.5f);
+            }
+            transform.position = avg;
+        }
+    }
 }
