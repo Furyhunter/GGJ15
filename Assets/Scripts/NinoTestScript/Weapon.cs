d28b5007265cb5f60ca94e36f6805b1249f146b9
Popolo <cvernino@tx.rr.com>
2015-01-24 10:48:06 -0600

    Various Fixes.

--- Assets/Scripts/NinoTestScript/Weapon.cs
+++ Assets/Scripts/NinoTestScript/Weapon.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Weapon : MonoBehaviour
 {
+	public enum AmmoType {rifle , shotgun , rocket , pistol};
+
 	public GameObject Ammunition; 
 	public int ProjCount;
 	public int ProjSpeed;
 	public int ProjSpread; 
 	public float RefireDelay;
+
+	public AmmoType Ammo;
 }
