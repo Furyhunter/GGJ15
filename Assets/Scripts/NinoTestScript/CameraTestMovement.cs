d08f76376f6928fb7ce1210fc4a85b1be0d41062
Popolo <cvernino@tx.rr.com>
2015-01-24 11:05:24 -0600

    initial camera control.

---
+++ Assets/Scripts/NinoTestScript/CameraTestMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTestMovement : MonoBehaviour
+{
+	
+	void Update()
+	{
+		transform.position = new Vector3(0, 0, 5 + 10 * Mathf.Sin(Time.time));
+	}
+}
