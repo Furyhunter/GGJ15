10725c35053aafa1a01be3fb276ae81bfded9f70
bwang95 <b.wang@utexas.edu>
2015-01-23 20:34:31 -0600

    Committing preliminary spawner scripts

---
+++ Assets/Scripts/AmmoSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoSpawner : MonoBehaviour
+{
+    public GameObject[] ammoCrates;
+    public Transform[] spawnPoints;
+
+	void Start()
+	{
+        Spawn();
+	}
+	
+	void Update()
+	{
+	
+	}
+
+    void Spawn()
+    {
+        for (int k = 0; k < spawnPoints.Length; k++)
+            Instantiate(ammoCrates[(int)(Random.value * ammoCrates.Length)],
+                spawnPoints[k].position, spawnPoints[k].rotation);
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public int[] ptValues;
+    public int points;
+
+    public Transform[] spawnPoints;
+    private int currentSpawnPoint;
+
+    void Start()
+    {
+        if (ptValues.Length != enemies.Length)
+            throw new Exception("Not all enemies have assigned point values");
+        currentSpawnPoint = 0;
+        Spawn();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Spawn()
+    {
+        int index = enemies.Length;
+
+        while (points > 0)
+        {
+            while (ptValues[index] > points)
+                index--;
+            int i = rval(index);
+            points -= ptValues[i];
+            spawnEnemy(i);
+        }
+    }
+
+    int rval(int to)
+    {
+        return (int)(UnityEngine.Random.value * to);
+    }
+
+    void spawnEnemy(int i)
+    {
+        currentSpawnPoint = (currentSpawnPoint + 1) % spawnPoints.Length;
+        
+        Instantiate(enemies[i], 
+            spawnPoints[currentSpawnPoint].position, 
+            spawnPoints[currentSpawnPoint].rotation);
+    }
+}
