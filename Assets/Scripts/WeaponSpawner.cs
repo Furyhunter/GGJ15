0a0f5282a979639ed0a02e9e60ae7c20351cc743
bwang95 <b.wang@utexas.edu>
2015-01-24 23:39:25 -0600

    Weapon spawner

---
+++ Assets/Scripts/WeaponSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSpawner : MonoBehaviour
+{
+    public GameObject[] weapons;
+    public Transform spawnPoint;
+
+    void Start()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+            Instantiate(weapons[(int)(Random.value * weapons.Length)],
+                spawnPoint.position, spawnPoint.rotation);
+    }
+}
