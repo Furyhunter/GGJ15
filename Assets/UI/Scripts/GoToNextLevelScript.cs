04fec826b6d13893a2c2d9d2c494af2996061c58
Ronald Kinard <furyhunter600@gmail.com>
2015-01-25 02:30:15 -0600

    Add initial main menu pass
    Also the splash screens transitions to main menu

---
+++ Assets/UI/Scripts/GoToNextLevelScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToNextLevelScript : MonoBehaviour
+{
+
+    public int nextLevelIndex = 1;
+
+    void GoToNextLevel()
+    {
+        Application.LoadLevel(nextLevelIndex);
+    }
+}
---
+++ Assets/UI/Scripts/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour
+{
+    void OpenPlayerDialog()
+    {
+        // Modal dialog
+        
+    }
+
+    void OpenOptionsDialog()
+    {
+        // Modal dialog
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+}
