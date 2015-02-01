de0751c449180004ac9f3042a1877404b8c33cec
bwang95 <b.wang@utexas.edu>
2015-01-31 21:50:54 -0600

    Add back to main menu script

---
+++ Assets/Scripts/BackToMainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    public double delay = 2.0;
+	
+	void Update()
+	{
+        delay -= Time.deltaTime;
+        if (delay <= 0)
+            Application.LoadLevel("MainMenu");
+	}
+}
