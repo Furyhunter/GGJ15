c6d7e2559082ac15022cc1dfce37b5d7353b94ea
Ronald Kinard <furyhunter600@gmail.com>
2015-02-06 16:14:34 -0600

    Lerp camera motion

--- Assets/Scripts/NinoTestScript/CameraTargetControl.cs
+++ Assets/Scripts/NinoTestScript/CameraTargetControl.cs
@@ -4,17 +4,22 @@ using System.Collections;
 public class CameraTargetControl : MonoBehaviour
 {
     private GameObject[] players;
-    void Update()
+
+    private Vector3 oldTarget = new Vector3();
+
+    void FixedUpdate()
     {
         players = GameObject.FindGameObjectsWithTag("Player");
         if (players.Length != 0)
         {	
         	Vector3 avg = players[0].transform.position;
+            var curPos = transform.position;
             for (int i = 1; i < players.Length; ++i)
             {
                 avg = Vector3.Lerp(avg, players[i].transform.position, 0.5f);
             }
-            transform.position = avg;
+            transform.position = Vector3.Lerp(transform.position, avg, .1f);
+            oldTarget = avg;
         }
     }
 }
